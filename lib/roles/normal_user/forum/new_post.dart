20ad73886a81c5fcbea782fb00374c9744b1e1b7
Nousheen <pnousheen729@gmail.com>
2025-02-20 20:22:37 +0530

    forum update

--- lib/roles/normal_user/forum/new_post.dart
+++ lib/roles/normal_user/forum/new_post.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:women_safety_framework/utils/color_utils.dart';
@@ -11,12 +12,22 @@ class CreatePostScreen extends StatefulWidget {
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
   final TextEditingController _postController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> _submitPost() async {
     String content = _postController.text.trim();
     if (content.isEmpty) return;
 
+    User? user = _auth.currentUser;
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('You must be logged in to comment')),
+      );
+      return;
+    }
+
     await FirebaseFirestore.instance.collection('forum_posts').add({
+      'userId': user.uid,
       'anonymousName': "Anonymous User",
       'content': content,
       'timestamp': FieldValue.serverTimestamp(),
